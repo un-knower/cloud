8240c146b9d0c387b3fc72a2f0a5147d57c4ab82
xiaoma- <qiankun1028@163.com>
2014-11-11 11:04:39 +0800

    修改grails版本改为最新版2.4.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -55,18 +55,18 @@ grails.project.dependency.resolution = {
 
     plugins {
         // plugins for the build system only
-        build ":tomcat:7.0.54"
-
+		build ':tomcat:7.0.55'
         // plugins for the compile step
-        compile ":scaffolding:2.1.2"
-        compile ':cache:1.1.7'
-        //compile ":asset-pipeline:1.8.11"
+		compile ':cache:1.1.8'
+		compile ':scaffolding:2.1.2'
+        //compile ':asset-pipeline:1.9.9'
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate4:4.3.5.4" // or ":hibernate:3.6.10.16"
+        runtime ":hibernate4:4.3.6.1" // or ":hibernate:3.6.10.16"
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
-
+//		compile "javax.validation:validation-api:1.1.0.Final"
+//		runtime "org.hibernate:hibernate-validator:5.0.3.Final"
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.7.4"
         //compile ":less-asset-pipeline:1.7.0"
